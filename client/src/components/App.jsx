a9f93a0a5f6b4b498c4573a303aef760c5664bbc
Rishabh <2710rismi@gmail.com>
2023-08-27 12:58:48 +0530

    small change

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,27 +1,19 @@
 import React from "react";
 import Bot from "./Bot/Bot";
-<<<<<<< HEAD
 import PdfPage from "./Documents_Page/pdfPage";
-=======
 import NavBar from "./NavBar"; 
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
->>>>>>> 8aadee1f436f212ce423626dab1ea24c2c1ce5b0
 
 function App() {
     return (
         <> 
-<<<<<<< HEAD
-            <Bot/>
-            <hr />
-            <PdfPage />
-=======
         <Router>
            <NavBar />
              <Routes>
              <Route path="/" element={<Bot />} />
+             <Route path="/pdf" element={<PdfPage />} />
              </Routes>
            </Router>
->>>>>>> 8aadee1f436f212ce423626dab1ea24c2c1ce5b0
         </>
 
     );
