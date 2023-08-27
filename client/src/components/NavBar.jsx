276a411e2aa64d9b90b0b9007c5216001c933905
suryapratap-01 <suryapratap.singh2021@iem.edu.in>
2023-08-27 21:39:24 +0530

    F4

--- client/src/components/NavBar.jsx
+++ client/src/components/NavBar.jsx
@@ -1,33 +1,33 @@
 import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faHome, faScaleBalanced,faGavel,faRightToBracket } from '@fortawesome/free-solid-svg-icons';
+import { faHome, faScaleBalanced, faGavel, faRightToBracket } from '@fortawesome/free-solid-svg-icons';
 import './NavBar.css';
 
 const NavBar = () => {
     return (
-     
+
         <nav className="Navbar_items">
             <h1 className="Navbar_heading">Samvidhan.Ai  <FontAwesomeIcon icon={faScaleBalanced} className="Navbar_logo" /></h1>
-              
+
             <ul className="Navbar_menu">
-              <li>
-                   <a className="Navbar_links">
-                   <FontAwesomeIcon icon={faHome} />  Home
-                   </a>
-                </li>
                 <li>
-                    <a className="Navbar_links">
-                    <FontAwesomeIcon icon={faGavel} />  Legal Laws
+                    <a href='!#'>
+                        <FontAwesomeIcon icon={faHome} />  Home
                     </a>
                 </li>
                 <li>
-                    <a className="Navbar_links">
-                    <FontAwesomeIcon icon={faRightToBracket} />  Login/SignUp
+                    <a href='!#'>
+                        <FontAwesomeIcon icon={faGavel} />  Legal Laws
+                    </a>
+                </li>
+                <li>
+                    <a href='!#'>
+                        <FontAwesomeIcon icon={faRightToBracket} />  Login/SignUp
                     </a>
                 </li>
             </ul>
         </nav>
-        
+
     )
 
 };
